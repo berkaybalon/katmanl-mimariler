ac8f39d4e3c07cfb96b5211d45229b8294a10fef
Bemar <Bemar@DESKTOP-5JOPI67>
2022-06-05 16:05:25 +0300

    .

---
+++ DataAccess/Concrete/Memory/MemoryProductDal.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Abstract;
+using Entity.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.Memory
+{
+    public class MemoryProductDal : IProductDal
+    {
+
+        List<Product> _products;
+
+        public MemoryProductDal()
+        {
+
+
+        }
+
+        public void Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Product GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
